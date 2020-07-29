ce43125e2870390bd51fb0b038bc4edae6349a9f
mdirshaddev <mdirshaddev@gmail.com>
2020-07-29 22:21:55 +0530

    Working on setup.

---
+++ backend/wsgi.py
@@ -0,0 +1,18 @@
+import os
+
+from django.core.wsgi import get_wsgi_application
+# heroku setup
+# from dj_static import Cling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# default settings
+application = get_wsgi_application()
+
+# # Settings for heroku
+# application = Cling(get_wsgi_application())
+
+# if os.getcwd() == '/app':
+#     from whitenoise.django import DjangoWhiteNoise
+#     application = DjangoWhiteNoise(application)
+
