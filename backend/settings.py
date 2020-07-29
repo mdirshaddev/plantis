44cbdd33756be64e1c0ef8db59be61c0c90f4eac
mdirshaddev <mdirshaddev@gmail.com>
2020-07-29 23:15:02 +0530

    atleast the django error logs is running.

--- backend/settings.py
+++ backend/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 CORS_ORIGIN_ALLOW_ALL=True
 
 # heroku settings
-if os.getcwd() == '/app':
+if os.getcwd() == '/':
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
