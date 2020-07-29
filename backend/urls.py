aa5c88d1ae1be1cdbc550beaa46fe652500f325e
mdirshaddev <mdirshaddev@gmail.com>
2020-07-29 23:28:02 +0530

    Finally moving in the right direction.

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
