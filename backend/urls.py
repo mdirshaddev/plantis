97865376bc7cd40243800c196e0174827d3b84c2
mdirshaddev <mdirshaddev@gmail.com>
2020-07-31 12:12:40 +0530

    Fixing the url of react page.

--- backend/urls.py
+++ backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('app/', include('app.urls')),
-    re_path('home/.*', TemplateView.as_view(template_name='index.html'))
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
