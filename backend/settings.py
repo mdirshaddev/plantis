0a36cff6d718c8bbbc6e85264fa6a984c733ac3d
mdirshaddev <mdirshaddev@gmail.com>
2020-08-02 19:57:05 +0530

    Fixing the databases code in settings.py.

--- backend/settings.py
+++ backend/settings.py
@@ -74,9 +74,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'NurseryDjangoApp',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'USER': 'ednmnildjpoosp',
+        'PASSWORD': '07e9c70f405de05f1cdae525b2cf7d0c323ca824af2effb2eb1a0d306d0335e7',
+        'HOST': 'ec2-18-235-109-97.compute-1.amazonaws.com',
     }
 }
 
