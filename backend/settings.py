ad267c09b9ff02737e13cb77f6eead38031f3d41
mdirshaddev <mdirshaddev@gmail.com>
2020-08-02 19:37:25 +0530

    Fixing the problem of heroku postgresql connection with django app.

--- backend/settings.py
+++ backend/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #         'HOST': 'localhost',
 #     }
 # }
+db_from_env = dj_database_url.config(conn_max_age=600)
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
